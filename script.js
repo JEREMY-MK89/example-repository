a3cfac1d7e90e8ed1903dde84dfe271ec2b2341e
moses <moses.kingi@moringaschool.com>
2023-09-26 22:06:05 +0300

    completed assigment, finished website

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+alert('Hello World Welcome!')
+alert('We use cookies to improve HubSpot’s site. Some cookies are necessary for our website and services to function properly. Other cookies are optional and help personalize your experience, including advertising and analytics. You can consent to all cookies, decline all optional cookies, or manage optional cookies. Without a selection, our default cookie settings will apply. You can change your preferences at any time. To learn more, check out our!')
